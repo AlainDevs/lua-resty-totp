3c6f7dcff8bb0379de13e11b01633d0a05d0606e
Alain Chan <43245915+AlainDevs@users.noreply.github.com>
2024-05-22 22:45:31 +0800

    Create totp.lua

---
+++ lib/resty/totp.lua
@@ -0,0 +1,136 @@
+-- load comment libs
+local require = require
+
+-- local function
+local ngx            = ngx
+local ngx_hmac_sha1  = ngx.hmac_sha1
+local ngx_time       = ngx.time
+local gsub           = ngx.re.gsub
+local bit_band       = bit.band
+local bit_lshift     = bit.lshift
+local bit_rshift     = bit.rshift
+local math_floor     = math.floor
+local math_random    = math.random
+local string_char    = string.char
+local string_format  = string.format
+local string_reverse = string.reverse
+local table_concat   = table.concat
+local table_insert   = table.insert
+-- Use the optimized `unpack` function from LuaJIT's FFI library
+local ffi = require("ffi")
+local base_encoding = require("resty.base_encoding")
+
+-- module define
+local _M = {
+    _VERSION = '0.02.01',
+}
+
+local function percent_encode_char(c)
+    return string_format("%%%02X", c:byte())
+end
+
+
+local function url_encode(str)
+    -- Use ngx.re.gsub for regex substitution
+    local r, _, err = gsub(str, "[^a-zA-Z0-9.~_-]", percent_encode_char)
+    if err then
+        ngx.log(ngx.ERR, "Failed to URL encode: ", err)
+        return nil, err
+    end
+    return r
+end
+
+
+local function totp_time_calc(ngx_time)
+    local time_str = ffi.new("uint8_t[8]")
+    for i = 7, 0, -1 do
+        time_str[i] = bit_band(ngx_time, 0xFF)
+        ngx_time = bit_rshift(ngx_time, 8)
+    end
+    return ffi.string(time_str, 8)
+end
+
+local function totp_new_key()
+    local tmp_k = {} -- Use a table to store bytes
+    math.randomseed(ngx_time())
+    for i = 1, 10 do
+        table_insert(tmp_k, string_char(math_random(0, 255)))
+    end
+    -- Concatenate the bytes efficiently with table.concat
+    return base_encoding.encode_base32(table_concat(tmp_k))
+end
+
+
+------ TOTP functions ------
+local TOTP_MT = {}
+
+
+function _M.totp_init(secret_key)
+    local m = {
+        type = "totp",
+    }
+    setmetatable(m, { __index = TOTP_MT, __tostring = TOTP_MT.serialize })
+    m:new_key(secret_key)
+    return m
+end
+
+
+function TOTP_MT:new_key(secret_key)
+    self.key = secret_key or totp_new_key()
+    self.key_decoded = base_encoding.decode_base32(self.key)
+end
+
+
+
+function TOTP_MT:calc_token(var_time)
+    local ngx_time = math_floor(var_time / 30)
+    local hmac_result = ngx_hmac_sha1(self.key_decoded, totp_time_calc(ngx_time))
+    
+    local HMAC_offset = bit_band(hmac_result:byte(20), 0xF) 
+    local TOTP_token = 0
+    for i = 1, 4 do
+        TOTP_token = TOTP_token + bit_lshift(hmac_result:byte(HMAC_offset + i), (4 - i) * 8)
+    end
+
+    TOTP_token = bit_band(TOTP_token, 0x7FFFFFFF)
+    TOTP_token = TOTP_token % 1000000
+    return string_format("%06d", TOTP_token)
+end
+
+
+
+function TOTP_MT:verify_token(token)
+    return (token == self:calc_token(ngx_time()))
+end
+
+
+function TOTP_MT:get_url(issuer, account)
+    return table_concat{
+        "otpauth://totp/",
+        account,
+        "?secret=", self.key,
+        "&issuer=", issuer,
+    }
+end
+
+
+function TOTP_MT:get_qr_url(issuer, account)
+    return table_concat{
+        "https://chart.googleapis.com/chart",
+        "?chs=", "200x200",
+        "&cht=qr",
+        "&chl=200x200",
+        "&chld=M|0",
+        "&chl=", url_encode(self:get_url(issuer, account)),
+    }
+end
+
+function TOTP_MT:serialize()
+    return table_concat{
+        "type:totp\n",
+        "secret:", self.key,
+        "secret_decoded", self.key_decoded,
+    }
+end
+
+return _M
